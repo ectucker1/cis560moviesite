050d4c25124ca2d1932e729aa583e232c6b47944
Ethan Tucker <ectucker1@gmail.com>
2022-04-10 21:55:09 -0500

    Added forms for the login and signup pages

--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,53 @@
 <template>
-  <Navbar/>
+  <div>
+    <Navbar/>
+
+    <b-container class="bg-light rounded p-5">
+      <h1 class="h3 mb-3 fw-normal">Sign In to Movie Site</h1>
+      <b-form>
+        <!--Email-->
+        <b-form-group
+          label="Email Address:"
+          label-for="login-email"
+        >
+          <b-form-input
+            id="login-email"
+            v-model="login.email"
+            type="email"
+            placeholder="Enter your account email address"
+            required
+          />
+        </b-form-group>
+        <!--Password-->
+        <b-form-group
+          label="Password:"
+          label-for="login-password"
+        >
+          <b-form-input
+            id="login-email"
+            v-model="login.password"
+            type="password"
+            placeholder="Enter your account password"
+            required
+          />
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Sign In</b-button>
+      </b-form>
+    </b-container>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'LoginPage'
+  name: 'LoginPage',
+  data() {
+    return {
+      login: {
+        email: '',
+        password: ''
+      },
+    }
+  }
 }
 </script>
--- pages/signup.vue
+++ pages/signup.vue
@@ -1,9 +1,67 @@
 <template>
-  <Navbar/>
+  <div>
+    <Navbar/>
+
+    <b-container class="bg-light rounded p-5">
+      <h1 class="h3 mb-3 fw-normal">Sign Up for Movie Site</h1>
+      <b-form>
+        <!--Email-->
+        <b-form-group
+          label="Email Address:"
+          label-for="signup-email"
+        >
+          <b-form-input
+            id="signup-email"
+            v-model="signup.email"
+            type="email"
+            placeholder="Enter your email address"
+            required
+          />
+        </b-form-group>
+        <!--Username-->
+        <b-form-group
+          label="Display Name:"
+          label-for="signup-username"
+        >
+          <b-form-input
+            id="signup-username"
+            v-model="signup.username"
+            type="text"
+            placeholder="Enter your name"
+            required
+          />
+        </b-form-group>
+        <!--Password-->
+        <b-form-group
+          label="Password:"
+          label-for="signup-password"
+        >
+          <b-form-input
+            id="signup-email"
+            v-model="signup.password"
+            type="password"
+            placeholder="Enter a password"
+            required
+          />
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Sign Up</b-button>
+      </b-form>
+    </b-container>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'SignupPage'
+  name: 'SignupPage',
+  data() {
+    return {
+      signup: {
+        email: '',
+        username: '',
+        password: ''
+      },
+    }
+  }
 }
 </script>
